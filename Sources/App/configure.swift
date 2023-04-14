b4a3308ecdead52e6c674025f4e4b2f6055f39de
Maksim Sashcheka <maksim.sashcheka@yellow.systems>
2023-04-14 15:58:12 +0300

    Remove sample todo model

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,7 +15,7 @@ public func configure(_ app: Application) throws {
         database: Environment.get("DATABASE_NAME") ?? "coingecko_database"
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
+    
     
     try app.autoMigrate().wait()
 
